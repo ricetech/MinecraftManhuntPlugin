db57e10ac2c9944c3ed5ce36151c821302caaf98
Eric C <39015447+ricetech@users.noreply.github.com>
2020-12-31 21:41:43 -0500

    Get plugin manager in onEnable

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/MinecraftManhuntPlugin.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/MinecraftManhuntPlugin.java
@@ -1,11 +1,13 @@
 package com.github.ricetech.minecraftmanhuntplugin;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MinecraftManhuntPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Manhunt Plugin enabled!");
+        PluginManager manager = getServer().getPluginManager();
     }
 
     @Override
