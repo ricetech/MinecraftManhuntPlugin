26071fa276de0b03b044e24e66ff8bf12f1cfad1
Eric C <39015447+ricetech@users.noreply.github.com>
2020-12-31 23:34:23 -0500

    Create InventoryHandlerListener.giveCompass()

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/InventoryHandlerListener.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/InventoryHandlerListener.java
@@ -1,6 +1,22 @@
 package com.github.ricetech.minecraftmanhuntplugin.listeners;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class InventoryHandlerListener implements Listener {
+    private void giveCompass(Player player) {
+        PlayerInventory inventory = player.getInventory();
+
+        // Do not add compass if player already has it
+        if (inventory.contains(Material.COMPASS)) {
+            return;
+        }
+
+        // Add compass to inventory
+        inventory.addItem(new ItemStack(Material.COMPASS, 1));
+    }
+
 }
