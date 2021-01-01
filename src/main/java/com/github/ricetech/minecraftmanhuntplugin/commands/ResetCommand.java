5bd2e750652914027229fd72208a8a930b5fac84
Eric C <39015447+ricetech@users.noreply.github.com>
2020-12-31 23:56:52 -0500

    Add basic implementation for ResetCommand

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/ResetCommand.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/ResetCommand.java
@@ -1,12 +1,33 @@
 package com.github.ricetech.minecraftmanhuntplugin.commands;
 
+import com.github.ricetech.minecraftmanhuntplugin.listeners.InventoryHandlerListener;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class ResetCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        if (args.length > 0) {
+            return false;
+        }
+
+        Location spawnLocation = Bukkit.getWorlds().get(0).getSpawnLocation();
+
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            // Clear inventory
+            p.getInventory().clear();
+
+            // Give compass
+            InventoryHandlerListener.giveCompass(p);
+
+            // Teleport to spawn
+            p.teleport(spawnLocation);
+        }
+
         return false;
     }
 }
