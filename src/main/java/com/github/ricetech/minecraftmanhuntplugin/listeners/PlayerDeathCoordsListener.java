dc813b47785e69f7b0befb7a097a1bf1cd47aca8
Eric C <39015447+ricetech@users.noreply.github.com>
2020-12-31 22:14:15 -0500

    Change player death message to show environment instead of world name

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/PlayerDeathCoordsListener.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/PlayerDeathCoordsListener.java
@@ -1,6 +1,7 @@
 package com.github.ricetech.minecraftmanhuntplugin.listeners;
 
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,7 +14,21 @@ public class PlayerDeathCoordsListener implements Listener {
     public void playerDeath(PlayerDeathEvent event) {
         Player p = event.getEntity();
         Location pLoc = p.getLocation();
+        World world = p.getWorld();
+        String worldEnv = world.getName();
+        //noinspection EnhancedSwitchMigration
+        switch (world.getEnvironment()) {
+            case NORMAL:
+                worldEnv = "the Overworld";
+                break;
+            case NETHER:
+                worldEnv = "the Nether";
+                break;
+            case THE_END:
+                worldEnv = "The End";
+                break;
+        }
         p.sendMessage("You died at: " + pLoc.getBlockX() + ", " + pLoc.getBlockY() + ", " + pLoc.getBlockZ() +
-                " in: " + Objects.requireNonNull(pLoc.getWorld()).getName());
+                " in " + worldEnv);
     }
 }
