b14060604811264f55b3e1cbc8bf990a2d9ddb9c
Eric C <39015447+ricetech@users.noreply.github.com>
2020-12-31 22:16:30 -0500

    Remove unused import

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/PlayerDeathCoordsListener.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/PlayerDeathCoordsListener.java
@@ -7,8 +7,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
-import java.util.Objects;
-
 public class PlayerDeathCoordsListener implements Listener {
     @EventHandler
     public void playerDeath(PlayerDeathEvent event) {
