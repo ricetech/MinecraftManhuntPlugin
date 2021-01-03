d9463e6189f587d728c7dfda36616146c4ba0f8d
Eric C <39015447+ricetech@users.noreply.github.com>
2021-01-03 17:09:45 -0500

    Add ScoreKeeper init code

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/data/ScoreKeeper.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/data/ScoreKeeper.java
@@ -1,5 +1,7 @@
 package com.github.ricetech.minecraftmanhuntplugin.data;
 
+import org.bukkit.Bukkit;
+import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 
@@ -7,4 +9,23 @@ public class ScoreKeeper {
     private Scoreboard mainScoreboard;
     private Objective kills;
     private Objective deaths;
+
+    public ScoreKeeper() {
+        //noinspection ConstantConditions
+        this.mainScoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+
+        // Create kills if it doesn't exist already
+        this.kills = mainScoreboard.getObjective("kills");
+        if (this.kills == null) {
+            this.kills = mainScoreboard.registerNewObjective("kills", "playerKillCount", "Kills");
+        }
+        kills.setDisplaySlot(DisplaySlot.PLAYER_LIST);
+
+        // Create deaths if it doesn't exist already
+        this.deaths = mainScoreboard.getObjective("deaths");
+        if (this.deaths == null) {
+            this.deaths = mainScoreboard.registerNewObjective("deaths", "deathCount", "Deaths");
+        }
+        deaths.setDisplaySlot(DisplaySlot.SIDEBAR);
+    }
 }
