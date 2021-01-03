cbfd0a8da35e8de7908c10472ef54bf2c890fb02
Eric C <39015447+ricetech@users.noreply.github.com>
2021-01-03 15:49:21 -0500

    Create DeathCounter.java

---
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/data/ScoreKeeper.java
@@ -0,0 +1,10 @@
+package com.github.ricetech.minecraftmanhuntplugin.data;
+
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+
+public class ScoreKeeper {
+    private Scoreboard mainScoreboard;
+    private Objective kills;
+    private Objective deaths;
+}
