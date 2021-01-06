19b409d963f47871e60c49b5ad4cc33c4d576c5a
Eric C <39015447+ricetech@users.noreply.github.com>
2021-01-05 21:08:01 -0500

    Create static variables for team colors

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/MinecraftManhuntPlugin.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/MinecraftManhuntPlugin.java
@@ -6,6 +6,7 @@ import com.github.ricetech.minecraftmanhuntplugin.data.ScoreKeeper;
 import com.github.ricetech.minecraftmanhuntplugin.data.TeamManager;
 import com.github.ricetech.minecraftmanhuntplugin.listeners.InventoryHandlerListener;
 import com.github.ricetech.minecraftmanhuntplugin.listeners.PlayerDeathCoordsListener;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,6 +14,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MinecraftManhuntPlugin extends JavaPlugin {
     public static final String COUNTDOWN_COMMAND_ALIAS = "countdown";
     public static final String RESET_COMMAND_ALIAS = "rs";
+
+    public static final ChatColor RUNNERS_COLOR = ChatColor.DARK_BLUE;
+    public static final ChatColor ELIMINATED_COLOR = ChatColor.DARK_AQUA;
+    public static final ChatColor HUNTERS_COLOR = ChatColor.RED;
+    public static final ChatColor SPECTATORS_COLOR = ChatColor.GOLD;
 
     private boolean gameInProgress = false;
 
