e127927fa0daec961df2487c3226e96aadb0a262
Eric C <39015447+ricetech@users.noreply.github.com>
2021-01-05 21:56:17 -0500

    Create NewGameCommand

---
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/NewGameCommand.java
@@ -0,0 +1,46 @@
+package com.github.ricetech.minecraftmanhuntplugin.commands;
+
+import com.github.ricetech.minecraftmanhuntplugin.MinecraftManhuntPlugin;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class NewGameCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String labels, String[] args) {
+        if (args.length != 0) {
+            return false;
+        }
+
+        ComponentBuilder builderSelectTeamMsg = new ComponentBuilder("Select a team:");
+
+        TextComponent runnersComponent = new TextComponent("[RUNNERS]");
+        runnersComponent.setColor(ChatColor.DARK_BLUE);
+        runnersComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "changeteam runners"));
+
+        TextComponent eliminatedComponent = new TextComponent("[ELIMINATED]");
+        eliminatedComponent.setColor(ChatColor.DARK_AQUA);
+        eliminatedComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "changeteam eliminated"));
+
+        TextComponent huntersComponent = new TextComponent("[HUNTERS]");
+        huntersComponent.setColor(ChatColor.RED);
+        huntersComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "changeteam hunters"));
+
+        TextComponent spectatorsComponent = new TextComponent("[SPECTATORS]");
+        spectatorsComponent.setColor(ChatColor.GOLD);
+        spectatorsComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "changeteam spectators"));
+
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            p.spigot().sendMessage(builderSelectTeamMsg.create());
+        }
+
+        return true;
+    }
+}
