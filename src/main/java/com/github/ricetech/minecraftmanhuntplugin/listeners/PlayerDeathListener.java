420f6529fd0c3d7e12fc4b00db83763b043f3a72
Eric C <39015447+ricetech@users.noreply.github.com>
2021-01-08 15:33:19 -0500

    Create PlayerDeathListener.sendDeathConfirmMsg()

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/PlayerDeathListener.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/PlayerDeathListener.java
@@ -1,6 +1,10 @@
 package com.github.ricetech.minecraftmanhuntplugin.listeners;
 
 import com.github.ricetech.minecraftmanhuntplugin.data.ScoreKeeper;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,6 +17,28 @@ public class PlayerDeathListener implements Listener {
         this.scoreKeeper = scoreKeeper;
     }
 
+    public static void sendDeathConfirmMsg(Player p) {
+        p.sendMessage("Did you die to natural causes or to a player?");
+        p.sendMessage("Remember, you must select [Player] even if the cause of death was indirect.");
+        ComponentBuilder builderDeathConfirmMsg = new ComponentBuilder("Pick one: ");
+
+        TextComponent naturalCausesComponent = new TextComponent("[Natural Causes]");
+        naturalCausesComponent.setColor(ChatColor.DARK_BLUE);
+        naturalCausesComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/"));
+
+        TextComponent playerComponent = new TextComponent("[Player]");
+        playerComponent.setColor(ChatColor.RED);
+        playerComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/"));
+
+        builderDeathConfirmMsg
+                .append(" ")
+                .append(naturalCausesComponent)
+                .append(" ")
+                .append(playerComponent);
+
+        p.spigot().sendMessage(builderDeathConfirmMsg.create());
+    }
+
     @EventHandler
     public void eliminationMsgOnDeath(PlayerDeathEvent event) {
         Player killer = event.getEntity().getKiller();
@@ -22,6 +48,7 @@ public class PlayerDeathListener implements Listener {
             // No other actions - teleports are not allowed after a death to another player
         } else {
             // Ask if this death occurred because of a player or not
+            sendDeathConfirmMsg(event.getEntity());
         }
     }
 }
