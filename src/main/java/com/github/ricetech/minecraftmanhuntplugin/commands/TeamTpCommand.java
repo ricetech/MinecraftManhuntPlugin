d3123126f268f67d015b40ea6a3118f08f943d97
Eric C <39015447+ricetech@users.noreply.github.com>
2021-01-26 23:22:23 -0500

    Add error for mismatched teams in TeamTpCommand

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/TeamTpCommand.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/TeamTpCommand.java
@@ -34,6 +34,12 @@ public class TeamTpCommand implements CommandExecutor {
         Player target = Bukkit.getPlayer(args[0]);
         if (target == null) {
             sender.sendMessage(ChatColor.RED + "Error: Target player does not exist or is offline");
+            return true;
+        }
+
+        if (teamManager.getTeam(p) != teamManager.getTeam(target)) {
+            sender.sendMessage(ChatColor.RED + "Error: Target player is not on your team");
+            return true;
         }
         return true;
     }
