5233537e9c611ea5c345c87584ff987796e62d5f
Eric C <39015447+ricetech@users.noreply.github.com>
2021-01-26 22:53:26 -0500

    Add safety checks to TeamTpCommand

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/TeamTpCommand.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/TeamTpCommand.java
@@ -1,12 +1,29 @@
 package com.github.ricetech.minecraftmanhuntplugin.commands;
 
+import com.github.ricetech.minecraftmanhuntplugin.MinecraftManhuntPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class TeamTpCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+        if (args.length != 1) {
+            return false;
+        }
+
+        if (!(commandSender instanceof Player)) {
+            MinecraftManhuntPlugin.sendOnlyPlayersErrorMsg(commandSender);
+            return true;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+        if (target == null) {
+            commandSender.sendMessage(ChatColor.RED + "Error: Target player does not exist or is offline");
+        }
         return true;
     }
 }
