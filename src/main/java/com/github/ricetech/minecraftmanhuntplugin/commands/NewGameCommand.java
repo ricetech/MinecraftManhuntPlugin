a8a3a2b4b61e408e1d694ccdee686d17b955b508
Eric C <39015447+ricetech@users.noreply.github.com>
2021-02-02 22:47:59 -0500

    Add set game in progress false to NewGameCommand

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/NewGameCommand.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/NewGameCommand.java
@@ -1,17 +1,27 @@
 package com.github.ricetech.minecraftmanhuntplugin.commands;
 
+import com.github.ricetech.minecraftmanhuntplugin.MinecraftManhuntPlugin;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class NewGameCommand implements CommandExecutor {
+    private final MinecraftManhuntPlugin manhuntPlugin;
+
+    public NewGameCommand(MinecraftManhuntPlugin manhuntPlugin) {
+        this.manhuntPlugin = manhuntPlugin;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String labels, String[] args) {
         if (args.length != 0) {
             return false;
         }
+
+        this.manhuntPlugin.setGameInProgress(false);
 
         for (Player p : Bukkit.getOnlinePlayers()) {
             TeamSwitchCommand.sendTeamSelectMsg(p);
