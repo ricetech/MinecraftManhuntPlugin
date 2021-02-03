3f2375f35049f593f84bcfc6098b9931b25ed93f
Eric C <39015447+ricetech@users.noreply.github.com>
2021-02-02 22:49:00 -0500

    Add broadcast message to StopGameCommand

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/StopGameCommand.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/StopGameCommand.java
@@ -1,6 +1,8 @@
 package com.github.ricetech.minecraftmanhuntplugin.commands;
 
 import com.github.ricetech.minecraftmanhuntplugin.MinecraftManhuntPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,6 +17,7 @@ public class StopGameCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         this.manhuntPlugin.setGameInProgress(false);
+        Bukkit.broadcastMessage(ChatColor.AQUA + "Manhunt: The game has ended.");
         return true;
     }
 }
