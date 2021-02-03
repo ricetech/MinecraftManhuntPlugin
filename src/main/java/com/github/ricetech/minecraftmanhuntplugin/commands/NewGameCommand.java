27f2fea5d8bda26dabf2607baef58c478bfc817e
Eric C <39015447+ricetech@users.noreply.github.com>
2021-02-02 22:00:43 -0500

    Move sendTeamSelectMsg into TeamSwitchCommand

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/NewGameCommand.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/NewGameCommand.java
@@ -1,9 +1,5 @@
 package com.github.ricetech.minecraftmanhuntplugin.commands;
 
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,32 +7,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class NewGameCommand implements CommandExecutor {
-    public static void sendTeamSelectMsg(Player p) {
-        ComponentBuilder builderSelectTeamMsg = new ComponentBuilder("Select a team:");
-
-        TextComponent runnersComponent = new TextComponent("[RUNNERS]");
-        runnersComponent.setColor(ChatColor.DARK_BLUE);
-        runnersComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/changeteam runners"));
-
-        TextComponent huntersComponent = new TextComponent("[HUNTERS]");
-        huntersComponent.setColor(ChatColor.RED);
-        huntersComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/changeteam hunters"));
-
-        TextComponent spectatorsComponent = new TextComponent("[SPECTATORS]");
-        spectatorsComponent.setColor(ChatColor.GOLD);
-        spectatorsComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/changeteam spectators"));
-
-        builderSelectTeamMsg
-                .append(" ")
-                .append(runnersComponent)
-                .append(" ")
-                .append(huntersComponent)
-                .append(" ")
-                .append(spectatorsComponent);
-
-        p.spigot().sendMessage(builderSelectTeamMsg.create());
-    }
-
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String labels, String[] args) {
         if (args.length != 0) {
@@ -44,7 +14,7 @@ public class NewGameCommand implements CommandExecutor {
         }
 
         for (Player p : Bukkit.getOnlinePlayers()) {
-            sendTeamSelectMsg(p);
+            TeamSwitchCommand.sendTeamSelectMsg(p);
         }
 
         return true;
--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/TeamSwitchCommand.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/TeamSwitchCommand.java
@@ -3,6 +3,10 @@ package com.github.ricetech.minecraftmanhuntplugin.commands;
 import com.github.ricetech.minecraftmanhuntplugin.MinecraftManhuntPlugin;
 import com.github.ricetech.minecraftmanhuntplugin.data.ManhuntTeam;
 import com.github.ricetech.minecraftmanhuntplugin.data.TeamManager;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,6 +21,32 @@ public class TeamSwitchCommand implements CommandExecutor {
     public TeamSwitchCommand(MinecraftManhuntPlugin manhuntPlugin) {
         this.teamManager = manhuntPlugin.getTeamManager();
         this.validTeams = teamManager.getValidTeams();
+    }
+
+    public static void sendTeamSelectMsg(Player p) {
+        ComponentBuilder builderSelectTeamMsg = new ComponentBuilder("Select a team:");
+
+        TextComponent runnersComponent = new TextComponent("[RUNNERS]");
+        runnersComponent.setColor(ChatColor.DARK_BLUE);
+        runnersComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/changeteam runners"));
+
+        TextComponent huntersComponent = new TextComponent("[HUNTERS]");
+        huntersComponent.setColor(ChatColor.RED);
+        huntersComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/changeteam hunters"));
+
+        TextComponent spectatorsComponent = new TextComponent("[SPECTATORS]");
+        spectatorsComponent.setColor(ChatColor.GOLD);
+        spectatorsComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/changeteam spectators"));
+
+        builderSelectTeamMsg
+                .append(" ")
+                .append(runnersComponent)
+                .append(" ")
+                .append(huntersComponent)
+                .append(" ")
+                .append(spectatorsComponent);
+
+        p.spigot().sendMessage(builderSelectTeamMsg.create());
     }
 
     @Override
