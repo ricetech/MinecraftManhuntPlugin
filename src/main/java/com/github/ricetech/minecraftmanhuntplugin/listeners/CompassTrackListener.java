b67e429a6ca037628e897994979b4a3293350703
Eric C <39015447+ricetech@users.noreply.github.com>
2021-07-21 15:54:54 -0400

    Prevent double-trigger of CompassTrackListener

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/CompassTrackListener.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/CompassTrackListener.java
@@ -7,15 +7,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class CompassTrackListener implements Listener {
     @EventHandler
     public void onCompassRightClick(PlayerInteractEvent event) {
         Player p = event.getPlayer();
 
-        if ((event.getAction() == Action.RIGHT_CLICK_AIR) &&
-                (p.getInventory().getItemInMainHand().getType() == Material.COMPASS ||
-                        p.getInventory().getItemInOffHand().getType() == Material.COMPASS)) {
+        if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) &&
+                ((p.getInventory().getItemInMainHand().getType() == Material.COMPASS && event.getHand() == EquipmentSlot.HAND) ||
+                        (p.getInventory().getItemInOffHand().getType() == Material.COMPASS && event.getHand() == EquipmentSlot.OFF_HAND))) {
             TrackCommand.handleCompassRightClick(p);
         }
     }
