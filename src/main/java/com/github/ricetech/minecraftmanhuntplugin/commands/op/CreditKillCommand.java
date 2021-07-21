15f99e46ea8a4bac69f0110f5e00835fd2062220
Eric C <39015447+ricetech@users.noreply.github.com>
2021-07-20 23:16:02 -0400

    Create CreditKillCommand

---
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/commands/op/CreditKillCommand.java
@@ -0,0 +1,41 @@
+package com.github.ricetech.minecraftmanhuntplugin.commands.op;
+
+import com.github.ricetech.minecraftmanhuntplugin.MinecraftManhuntPlugin;
+import com.github.ricetech.minecraftmanhuntplugin.data.ScoreKeeper;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class CreditKillCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        if (args.length != 1) {
+            return false;
+        }
+
+        Player p;
+
+        if (!(sender instanceof Player)) {
+            MinecraftManhuntPlugin.sendOnlyPlayersErrorMsg(sender);
+            return true;
+        } else {
+            p = ((Player) sender);
+        }
+
+        Player playerToCredit = Bukkit.getPlayer(args[0]);
+
+        if (playerToCredit == null) {
+            MinecraftManhuntPlugin.sendErrorMsg(sender, "Target player does not exist or is offline.");
+            return true;
+        }
+
+        ScoreKeeper.addKill(playerToCredit);
+        playerToCredit.sendMessage(ChatColor.DARK_AQUA + "You have been credited a kill by " + p.getName());
+
+        return true;
+    }
+}
