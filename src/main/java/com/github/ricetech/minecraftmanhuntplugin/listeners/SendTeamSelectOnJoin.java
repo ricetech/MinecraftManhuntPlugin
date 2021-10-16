b710a22457683089c5aff4fad96f9da282a6be82
Eric C <39015447+ricetech@users.noreply.github.com>
2021-10-16 18:44:26 -0400

    Only offer team select on join if player is not on a team

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/SendTeamSelectOnJoin.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/SendTeamSelectOnJoin.java
@@ -14,19 +14,22 @@ public class SendTeamSelectOnJoin implements Listener {
     public void sendTeamSelectOnJoin(PlayerJoinEvent event) {
         Player p = event.getPlayer();
 
-        if (MinecraftManhuntPlugin.isGameInProgress) {
-            TeamManager.editTeam(p, ManhuntTeam.SPECTATORS);
-            p.sendMessage(MinecraftManhuntPlugin.GAME_MSG_COLOR + "Welcome to Manhunt!");
-            p.sendMessage(MinecraftManhuntPlugin.GAME_MSG_COLOR + "A game is in progress. With the permission of the other players, " +
-                    "feel free to use the message below to join a team. You can also continue watching as a spectator " +
-                    "without selecting a team.");
-            TeamSwitchCommand.sendTeamSelectMsg(p);
-        } else if (MinecraftManhuntPlugin.isTeamSelectInProgress) {
-            p.sendMessage(MinecraftManhuntPlugin.GAME_MSG_COLOR + "Welcome to Manhunt!");
-            p.sendMessage(MinecraftManhuntPlugin.GAME_MSG_COLOR + "Team Select is currently in progress. " +
-                    "Please join a team using the message below.");
-            TeamSwitchCommand.sendTeamSelectMsg(p);
-        }
+        ManhuntTeam team = TeamManager.getTeam(p);
 
+        if (team == null) {
+            if (MinecraftManhuntPlugin.isGameInProgress) {
+                TeamManager.editTeam(p, ManhuntTeam.SPECTATORS);
+                p.sendMessage(MinecraftManhuntPlugin.GAME_MSG_COLOR + "Welcome to Manhunt!");
+                p.sendMessage(MinecraftManhuntPlugin.GAME_MSG_COLOR + "A game is in progress. With the permission of the other players, " +
+                        "feel free to use the message below to join a team. You can also continue watching as a spectator " +
+                        "without selecting a team.");
+                TeamSwitchCommand.sendTeamSelectMsg(p);
+            } else if (MinecraftManhuntPlugin.isTeamSelectInProgress) {
+                p.sendMessage(MinecraftManhuntPlugin.GAME_MSG_COLOR + "Welcome to Manhunt!");
+                p.sendMessage(MinecraftManhuntPlugin.GAME_MSG_COLOR + "Team Select is currently in progress. " +
+                        "Please join a team using the message below.");
+                TeamSwitchCommand.sendTeamSelectMsg(p);
+            }
+        }
     }
 }
