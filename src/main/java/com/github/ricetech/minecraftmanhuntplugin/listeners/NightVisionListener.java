4ea3c90ce93407762375246a43477b1530c2ab4a
Eric C. <39015447+ricetech@users.noreply.github.com>
2024-10-11 01:36:00 -0400

    Fix Night Vision applying too early

--- src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/NightVisionListener.java
+++ src/main/java/com/github/ricetech/minecraftmanhuntplugin/listeners/NightVisionListener.java
@@ -1,20 +1,25 @@
 package com.github.ricetech.minecraftmanhuntplugin.listeners;
 
-import org.bukkit.Material;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 @SuppressWarnings("unused")
 public class NightVisionListener implements Listener {
-    public static void applyNightVision(Player player) {
-        player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 0, false, false));
+    private final JavaPlugin plugin;
+
+    public NightVisionListener(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void applyNightVision(Player player) {
+        Bukkit.getScheduler().runTaskLater(plugin, () -> player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 0, false, false)), 1);
     }
 
     @EventHandler
